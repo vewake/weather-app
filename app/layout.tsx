c10800ac79cd7f4d28cce9ec8a97e248a157b85c
vivek patil <17vivekpatil@gmail.com>
2024-10-08 23:41:58 +0530

    added force desktop mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <meta name="viewport" content="width=1024" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
